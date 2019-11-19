a3ae6cd204fd1461112ae941173521c21389cf49
Razaul Munir <razaulmunir@gmail.com>
2019-11-19 23:27:34 +0600

    initialize AndroidNetworking in onCreate()

--- app/src/main/java/org/myself/jsonpractice/MainActivity.java
+++ app/src/main/java/org/myself/jsonpractice/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.androidnetworking.AndroidNetworking;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        //https://raw.githubusercontent.com/RazM10/Data/master/DummyData.json
     }
 }
